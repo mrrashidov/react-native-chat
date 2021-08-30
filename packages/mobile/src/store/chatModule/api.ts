e04b4ceaae1a92f798116acf5855991165e9af4e
Rashidov Shoxrux <dev.rashidov@gmail.com>
2021-08-30 13:46:55 +0300

    fixed prefix api endpoint

--- packages/mobile/src/store/chatModule/api.ts
+++ packages/mobile/src/store/chatModule/api.ts
@@ -1,15 +1,16 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import axios from "axios";
 
-const _http = 'http://192.168.0.27:9000/chat'
+const _http = 'http://192.168.0.27:9000'
 
 export const fetchRoom = createAsyncThunk('room/fetchRoom', async ({userId, roomId}: any) => {
-    return await axios.get(`${_http}/${userId}/${roomId}/message`).then(res => res.data);
+    return await axios.get(`${_http}/chat/${userId}/${roomId}/message`).then(res => res.data);
 });
 
 export const fetchContacts = createAsyncThunk('contacts/fetchContacts', async (userId: number) => {
-    return await axios.get(`${_http}/${userId}`).then(res => res.data);
+    return await axios.get(`${_http}/chat/${userId}`).then(res => res.data);
 });
-export const sendDevice = async (payload: string | undefined) => {
+export const sendDevice = async (payload: object) => {
+	console.log('payload',payload)
     return await axios.post(`${_http}/device`,payload).then(res => res.data);
 }
